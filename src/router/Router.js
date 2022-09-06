bcebf984cbc072b4703ae4ce9c1422327c480f73
Akaarit-Frontend <mehedi.emon@akaarit.com>
2022-09-06 13:31:31 +0600

    fix

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Routes, Route } from "react-router-dom";
+import React,{useLayoutEffect} from 'react';
+import {Routes, Route, useLocation } from "react-router-dom";
 import Home from '../pages/Home';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
@@ -7,14 +7,24 @@ import Pricing from '../pages/Pricing';
 
 
 const routes = () => {
+  const Wrapper = ({children}) => {
+    const location = useLocation();
+    useLayoutEffect(() => {
+      document.documentElement.scrollTo(0, 0);
+    }, [location.pathname]);
+    return children
+  }
+
   return (
     <>
-      <Routes>
-        <Route exact path='/' element={<Home/>}/>
-        <Route exact path='/sign-in' element={<SignIn/>}/>
-        <Route exact path='/sign-up' element={<SignUp/>}/>
-        <Route exact path='/pricing' element={<Pricing/>}/>
-      </Routes>
+      <Wrapper>
+        <Routes>
+          <Route exact path='/' element={<Home/>}/>
+          <Route exact path='/sign-in' element={<SignIn/>}/>
+          <Route exact path='/sign-up' element={<SignUp/>}/>
+          <Route exact path='/pricing' element={<Pricing/>}/>
+        </Routes>
+      </Wrapper>
     </>
   )
 }
